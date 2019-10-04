897f07475765212c41aca5fec9ed8a9cdd37d48f
AJ <ascott1083@yahoo.com>
2019-10-04 11:58:03 -0500

    added switch commands

--- liri.js
+++ liri.js
@@ -16,3 +16,31 @@ let userCommand=process.argv[2];
 //Creates user input.
 let userInput=process.argv.splice(3,process.argv.length).join(' ');
 
+//Program conditions 
+switch (userCommand) {
+    // help function to clarify commands used
+    case "help":
+        console.log("Please type one of these commands\n"+
+                    "'concert-this': to search your favorite artist concerts\n"+
+                    "'spotify-this-song': to search your favorite song\n"+
+                    "'movie-this': to search your favorite movie \n"+
+                    "'do-what-it-says': using command from random.txt \n"
+                    );
+        break;
+    case "concert-this":
+        myConcert(userInput);
+        break;
+    case "spotify-this-song":
+        mySpotify(userInput);
+        break;
+    case "movie-this":
+        myMovies(userInput);
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+    //if anything else written
+    default:
+        console.log("LIRI doesn't understand that - Please type 'node liri.js help' for more information");
+};
+
