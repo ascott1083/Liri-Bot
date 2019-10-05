4e17f45b4ccfb8d1fd1bbd12ce73d2e518a8d748
AJ <ascott1083@yahoo.com>
2019-10-04 23:40:04 -0500

    added s to concert.js

---
+++ concerts.js
@@ -0,0 +1,33 @@
+// Requiring our spotify, OMDB, and bands in town modules exported from keys.js
+let keys = require("./keys");
+//Require data from File System npm package
+let fs = require("fs");
+//Require data from Axios npm package
+let axios = require("axios");
+//Require data from moment npm package
+let moment = require('moment');
+
+function myConcert(userInput) {
+    let artist = userInput;
+    let url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsInTown.id;
+
+    axios.get(url).then(
+        function (response) {
+            for (let i = 0; i < response.data.length; i++) {
+                console.log("Concert Time: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'));
+                console.log("Concert Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                console.log("Concert Venue: " + response.data[i].venue.name);
+                console.log('--------------------------------------------------')
+                fs.appendFileSync('log.txt', "\r\n" + "Concert Search Log----------------------" + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Name: " + response.data[i].venue.name + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Time: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A') + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "-----------------------------------------"+ "\r\n", 'utf8');
+            }
+        }
+    );
+
+};
+
+// Exporting the function which we will use in liri.js
+module.exports = myConcert;
